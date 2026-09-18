b8351703378bc61990e26c3493eced6fcfccb460
Andrei Takarski <atokarskiy@alfabank.ru>
2026-09-18 17:16:56 +0300

    Fix PWA refresh and iPhone safe area

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './src/App'
 import './src/styles/global.css'
+import { registerSW } from 'virtual:pwa-register'
+
+registerSW({ immediate: true })
 
 createRoot(document.getElementById('root')).render(
   React.createElement(React.StrictMode, null, React.createElement(App)),
--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
+      injectRegister: false,
       manifest: false,
       workbox: {
         cleanupOutdatedCaches: true,
